f2ccb9f3b4cf27bce1c9011fe0585a1a0e6aa149
chatelao <2461532+chatelao@users.noreply.github.com>
2026-07-11 22:04:39 +0000

    feat: add filteredBEMF to high-speed CSV logging
    - Updated MotorControl.cpp to include filteredBEMF in CSV output
    - Updated SerialConsole.cpp with new CSV header
    - Updated BETTER_CONFIG.md and HIGH_SPEED_LOGGING.md documentation
    - Updated test_main.cpp to match new CSV header format

--- xDuinoRails_MM/MotorControl.cpp
+++ xDuinoRails_MM/MotorControl.cpp
@@ -268,9 +268,9 @@ void MotorControl::update(int pwm, MM2DirectionState dir) {
           lastAdjustment = ((long)error * K / 16) + ((long)bemfErrorSum * I / 64);
 
           if (logger.isHighSpeedEnabled()) {
-            logger.printf(LogCategory::HighSpeed, "CSV | %lu | %d | %d | %d | %ld | %d | %d | %d | %d\n",
-                          now, targetPwm, currentBEMF, error, bemfErrorSum,
-                          lastAdjustment, targetPwm + (int)lastAdjustment, K, I);
+            logger.printf(LogCategory::HighSpeed, "CSV | %lu | %d | %d | %d | %d | %ld | %d | %d | %d | %d\n",
+                          now, targetPwm, currentBEMF, filteredBEMF, error,
+                          bemfErrorSum, lastAdjustment, targetPwm + (int)lastAdjustment, K, I);
           }
         }
         finalPwm += lastAdjustment;
